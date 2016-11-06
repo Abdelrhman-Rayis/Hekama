edefaabf8e26f71436035b315d273a4a3e6be353
rayis <rayis1@hotmail.com>
2016-11-06 14:25:13 +0300

    finshing the secound activity of good person and setting up the action listner for the buttons
    i only try to chnage the text in the bad button for testing and i set up
    the xml file with the image and the button for the secound activity

--- app/src/main/java/com/example/user/hekama/MainActivity.java
+++ app/src/main/java/com/example/user/hekama/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.hekama;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     final   Button goodButton =  (Button) (findViewById(R.id.goodButton));
+        final String gHekama = "When you give advice try to be cool and alone with out embarrassing the person .";
+      final   Button badButton =    (Button)  (findViewById(R.id.badButton));
+
+        goodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                badButton.setText(gHekama);
+            }
+        });
     }
 }
