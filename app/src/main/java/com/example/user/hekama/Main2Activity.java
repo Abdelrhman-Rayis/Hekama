e102cd8d60eba1ad3afb0e08d6c5ffec4eb6c6e8
rayis <rayis1@hotmail.com>
2016-11-06 17:51:14 +0300

    3 main changes
    first i fixed the puding in activity_main2.xml for the text view
    secound i have two string good and bad hekama then i created action
    listenr for my good advice button so it claa goTogood() method that
    create an intent and then send the data from the mainactivity class to
    main2activity class by the putExtra() method data in this case is pair
    vlaue the key is "goodHekama" and the vlaue is gHekama string then i
    start the activity.
    Third i created an instance of class bundle so i can get date from the
    intent that started the activity and i but that data in string called
    recivedHekama by the key "good Hekama"
    then i crated an object of textview and linked with textview in xml
    layout of the activity finally i set the text  from the text
    recivedHekama :+1:

--- app/src/main/java/com/example/user/hekama/Main2Activity.java
+++ app/src/main/java/com/example/user/hekama/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.user.hekama;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends Activity {
 
@@ -9,5 +10,12 @@ public class Main2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Bundle pickedData = getIntent().getExtras();
+        if(pickedData == null){
+            return;
+        }
+        String recivedHekama = pickedData.getString("good Hekama");
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(recivedHekama);
     }
 }
--- app/src/main/java/com/example/user/hekama/MainActivity.java
+++ app/src/main/java/com/example/user/hekama/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.hekama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,15 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-     final   Button goodButton =  (Button) (findViewById(R.id.goodButton));
-        final String gHekama = "When you give advice try to be cool and alone with out embarrassing the person .";
-      final   Button badButton =    (Button)  (findViewById(R.id.badButton));
+        final   Button goodButton =  (Button) (findViewById(R.id.goodButton));
+        final String gHekama = "When you give advice try to be cool and alone with out embarrassing the person  . (;";
+        final String bHekama = "Be smart don't try to show you'er righ or you wont to judge him or her :) life is not black or white";
+        final   Button badButton =    (Button)  (findViewById(R.id.badButton));
 
         goodButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                badButton.setText(gHekama);
+               // badButton.setText(gHekama);
+                goTogood(gHekama);
             }
         });
+
+    }
+    private void goTogood(String hekama){
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("good Hekama",hekama);
+        startActivity(intent);
     }
 }
